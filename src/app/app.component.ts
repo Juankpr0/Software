d7a1c4c8b85bacad3913a48377ac8204e7904d04
Juankpr0 <camitrianasuarez@gmail.com>
2024-12-06 22:25:38 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CuerpoComponent } from './admin/home/cuerpo/cuerpo.component';
+import { HeaderComponent } from './admin/home/header/header.component';
+import { FooterComponent } from './admin/home/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CuerpoComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
